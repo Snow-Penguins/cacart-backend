29a4bcb1d2bd74d4183f71debd91bc8fa3588f96
Daechul <dlee@nuundigital.com>
2024-03-28 04:36:26 -0600

    feat for #58 - allow cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  /**
+   * TODO: Allow only speific ports
+   */
+  app.enableCors()
+  await app.listen(8000);
 }
 bootstrap();
