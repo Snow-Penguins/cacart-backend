8bcfa3f4d9b9d697bf36da2ddc2fd8b99816a72e
JIYEON-H <jiyeon.heo0906@gmail.com>
2024-04-10 00:28:32 -0600

    Changed package manager to yarn

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,11 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'prettier',
   ],
   root: true,
   env: {
@@ -21,11 +22,21 @@ module.exports = {
       'error',
       {
         endOfLine: 'auto', // Automatically set the end of line to LF or CRLF
+        tabWidth: 2, // 2 spaces
       },
     ],
+    'quotes': ['error', 'single'], // ""=>''
+    'no-empty': 'error', // No empty in bracket
+    'indent': ['error', 2], // 2 spaces
+    'object-curly-spacing': ['error', 'always'], // space between { },
+    'no-multi-spaces': 'error', // Ex) var a =  1 => var a = 1
+    'no-unused-vars': 'off', // off in order to use @typescript-eslint/no-unused-vars
+    'space-before-blocks': 'error', // Ex) if (a){ => if (a) {
+    'no-trailing-spaces': 'error', // No trailing spaces
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { 'varsIgnorePattern': '^_use', 'argsIgnorePattern': '^_' }],
   },
 };
